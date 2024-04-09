854cd9584bbae828d1d2978ca88eee3c650347bd
= <=>
2024-04-10 04:57:22 +0530

    update

--- train.py
+++ train.py
@@ -807,3 +807,11 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
 
 # %%
 make_confusion_matrix(y_true, y_pred, classes=CLASSES, figsize=(20, 20), text_size=20)
+
+
+
+
+# %%
+print('1')
+
+print(2)
